1aa0c946b9f171aae265460f1fce0245d53ca41f
Saisankar99-dev <saisankarreddyputta@gmail.com>
2025-01-08 17:02:36 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+console.log("Hello, Jenkins from repository!");
+const numbers = [1, 2, 3, 4, 5];
+const sum = numbers.reduce((acc, curr) => acc + curr, 0);
+console.log(`The sum of [1, 2, 3, 4, 5] is: ${sum}`);
+
